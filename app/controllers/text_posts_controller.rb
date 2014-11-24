5c08f7fb0529857c24e6e733ca88636276521aa1
Anthony Lewis <me@anthonylewis.com>
2014-11-23 20:57:00 -0600

    Chapter 11

--- app/controllers/text_posts_controller.rb
+++ app/controllers/text_posts_controller.rb
@@ -15,6 +15,20 @@ class TextPostsController < ApplicationController
     end
   end
 
+  def edit
+    @text_post = current_user.text_posts.find(params[:id])
+  end
+
+  def update
+    @text_post = current_user.text_posts.find(params[:id])
+
+    if @text_post.update(text_post_params)
+      redirect_to post_path(@text_post), notice: "Post updated!"
+    else
+      render :edit, alert: "Error updating post."
+    end
+  end
+
   private
 
   def text_post_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :sessions
 
   get 'signup', to: 'users#new', as: 'signup'
-  get 'follow/:id', to: 'users#follow', as: 'follow_user'
+  post 'follow/:id', to: 'users#follow', as: 'follow_user'
 
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
